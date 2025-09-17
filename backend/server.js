bed9ef24e7da43e792943abca3415b6fe2e5e78d
SohitBennett <E22CSEU1409@bennett.edu.in>
2025-09-17 15:03:39 +0530

    Added auth routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 // start server
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = require('./src/app')
 const connectDB = require('./src/db/db');
 
 connectDB();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=>{
     console.log("server is running on port", port)
