11f3e3820aade44b5f3bec9126f6144ec0c9c02e
SohitBennett <E22CSEU1409@bennett.edu.in>
2025-09-24 03:02:42 +0530

    Added login and logout api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -44,9 +44,52 @@ async function registerUser(req, res){
 }
 
 async function loginUser(req, res){
+    const {email, password} = req.body;
+
+    const user = await userModel.findOne({
+        email
+    })
+
+    if(!user){
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    }, JWT_SECRET)
+
+    res.cookie("token", token); 
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
+
+}
+
+function logoutUser(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "User logged out sucessfully."
+    })
 }
 
 module.exports = {
     registerUser, 
-    loginUser
+    loginUser, 
+    logoutUser
 }
